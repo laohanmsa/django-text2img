2bba9610bb49674cd67067f45ee9017e47eaefe8
jiaxin <edison7500@gmail.com>
2018-05-23 23:15:12 +0800

    update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+import text2img
+
+install_requires = [
+                       'Django >= 1.11',
+                       'Pillow >= 5.1',
+                   ],
+
+setup(
+    name='django-text2img',
+    version=text2img.__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+)
