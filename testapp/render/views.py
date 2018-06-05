69e4e3806f32f003882119a5eface4dd3746afb9
Y4n <siyu@blocks.tech>
2018-06-05 13:58:06 +0800

    add 24 hour render

--- testapp/render/views.py
+++ testapp/render/views.py
@@ -12,12 +12,28 @@ class RenderTextView(View):
 
     def post(self, request, *args, **kwargs):
         json_data = json.loads(request.body)
-        if self.request.GET.get('type') == '24h':
-            r = RenderText(**json_data)
-            content = r.draw_24h_image_output()
+        r = RenderText(**json_data)
+        content = r.draw_image_output()
+        return HttpResponse(content=content, content_type='image/jpeg')
+
+    @csrf_exempt
+    def dispatch(self, request, *args, **kwargs):
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
         else:
-            r = RenderText(**json_data)
-            content = r.draw_image_output()
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+
+class RenderListTextView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("need post json data")
+
+    def post(self, request, *args, **kwargs):
+        json_data = json.loads(request.body)
+        r = RenderText(**json_data)
+        content = r.draw_24h_image()
         return HttpResponse(content=content, content_type='image/jpeg')
 
     @csrf_exempt
--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from render.views import RenderTextView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^render/?$', RenderTextView.as_view(), name='render_text'),
+    # url(r'^render/?$', RenderTextView.as_view(), name='render_text'),
+    url(r'^render/', include('render.urls', namespace='render')),
 ]
